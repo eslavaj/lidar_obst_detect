0a61b952739bc9eb9c25b5dc373de834d583ecae
Jose ESLAVA <joseslava@gmail.com>
2019-09-17 11:01:27 +0200

    kdtree algorithm implementation

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -29,8 +29,32 @@ struct KdTree
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
+		int depth = 0;
+		Node** current_node = &root;
 
+		while(1)
+		{
+			if(*current_node==NULL)
+			{
+				*current_node = new Node(point, id);
+				break;
+			}
+			else
+			{
+				int comparation_dim = depth % (point.size());
+				if( point[comparation_dim] >= (*current_node)->point[comparation_dim])
+				{
+					current_node = &((*current_node)->right);
+				}
+				else
+				{
+					current_node = &((*current_node)->left);
+				}
+			}
+			depth++;
+		}
 	}
+
 
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
